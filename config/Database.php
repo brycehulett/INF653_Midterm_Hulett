7eb8093833ee836601dd10d7355988e006e64a2c
bryce hulett <bryce.hulett@hotmail.com>
2022-03-06 16:19:43 -0600

    update for heroku

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,7 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
+    private $host = '127.0.0.1';
     private $db_name = 'quotesdb';
     private $username = 'root';
     private $password = '';
