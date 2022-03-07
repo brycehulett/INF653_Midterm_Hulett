ec55b3c4548a8ae714956518fca4d474ded0f110
bryce hulett <bryce.hulett@hotmail.com>
2022-03-06 22:13:30 -0600

    shortcut icon error

--- api/authors/index.php
+++ api/authors/index.php
@@ -1,7 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    //header('Content-Type: text');
+    //header('Content-Type: application/json');
+    header('Content-Type: text');
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method === 'OPTIONS') {
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
