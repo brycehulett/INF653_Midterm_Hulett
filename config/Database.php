8d07c73107b4d2a189979ccd0eb675ab4ebcc9ff
bryce hulett <bryce.hulett@hotmail.com>
2022-03-06 18:25:45 -0600

    added secured pswd & updated for jawsdb

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,22 @@
 <?php 
+  
+  require_once realpath(__DIR__ . '/vendor/autoload/.php');
+  use Dotenv\Dotenv;
+  $dotenv = Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
   class Database {
+    
     // DB Params
-    private $host = '127.0.0.1';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
+    private $host = 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $db_name = 'mm0a8iyg1d28s85z';
+    private $username = 'ygi5gl3vy9z8gali';
+    private $password;
     private $conn;
 
     // DB Connect
     public function connect() {
       $this->conn = null;
+      $this->password = getenv("PASSWORD");
 
       try { 
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
 echo
 "<h1>Midterm Project Hulett</h1>";
 ?>
