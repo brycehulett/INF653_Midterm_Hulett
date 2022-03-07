6b1f2a73bdef2580a2e83336eb921b3577c8a0cb
bryce hulett <bryce.hulett@hotmail.com>
2022-03-07 00:30:38 -0600

    test

--- api/authors/index.php
+++ api/authors/index.php
@@ -1,12 +1,13 @@
 <?php
     cors();
     header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: *");
     header('Content-Type: application/json');
     //header('Content-Type: text');
     //$method = $_SERVER['REQUEST_METHOD'];
    // if ($method === 'OPTIONS') {
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        //header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     //}
 
     include_once '../../config/Database.php';
