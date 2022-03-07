c57915690486043bc17131ef19077694d3625254
bryce hulett <bryce.hulett@hotmail.com>
2022-03-06 21:53:17 -0600

    removed 15 headers

--- api/authors/index.php
+++ api/authors/index.php
@@ -1,6 +1,6 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    //header('Content-Type: application/json');
+    header('Content-Type: application/json');
     header('Content-Type: text');
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method === 'OPTIONS') {
