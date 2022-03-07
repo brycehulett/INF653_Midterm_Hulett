e6ee56c0daaf39a30dc64b6ff19c24000d18b47e
bryce hulett <bryce.hulett@hotmail.com>
2022-03-06 20:10:36 -0600

    deleted vlucas

--- config/Database.php
+++ config/Database.php
@@ -1,30 +1,44 @@
 <?php 
   
-  require_once realpath(__DIR__ . '/vendor/autoload/.php');
-  use Dotenv\Dotenv;
-  $dotenv = Dotenv::createImmutable(__DIR__);
-  $dotenv->load();
   class Database {
-    
-    // DB Params
-    private $host = 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    private $db_name = 'mm0a8iyg1d28s85z';
-    private $username = 'ygi5gl3vy9z8gali';
+
+    private $hostname;
+    private $database;
+    private $username;
     private $password;
+
+    // DB Params
+    // private $host = 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    // private $db_name = 'mm0a8iyg1d28s85z';
+    // private $username = 'ygi5gl3vy9z8gali';
+    // private $password;
+    
     private $conn;
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
-      $this->password = getenv("PASSWORD");
-
-      try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
+      $url = getenv('JAWSDB_URL');
+      $dbparts = parse_url($url);
+  
+      $hostname = $dbparts['host'];
+      $username = $dbparts['user'];
+      $password = $dbparts['pass'];
+      $database = ltrim($dbparts['path'],'/');
+      //You cannot do the above for your local dev environment, just Heroku
+  
+      // Create your new PDO connection here
+      // This is also from the Heroku docs showing the PDO connection: 
+      try {
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
       }
-
-      return $this->conn;
+      catch(PDOException $e)
+      {
+        echo "Connection failed: " . $e->getMessage();
+      }
+      // We used this PDO connection format in previous weeks - reference w3schools.com
     }
   }
+  
