791a3620636ce5862d7c5512d2bcc13d249a1233
bryce hulett <bryce.hulett@hotmail.com>
2022-03-06 21:56:20 -0600

    added options in root

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    //header('Content-Type: text');
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    }
 echo
 "<h1>Midterm Project Hulett</h1>";
 ?>
