fa94960bb25d2145f4129281e6b697d9ac5c3c61
bryce hulett <bryce.hulett@hotmail.com>
2022-03-07 18:55:10 -0600

    updated headers removed cors functions

--- api/authors/index.php
+++ api/authors/index.php
@@ -1,14 +1,12 @@
 <?php
-    cors();
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: *");
     header('Content-Type: application/json');
-    //header('Content-Type: text');
-    //$method = $_SERVER['REQUEST_METHOD'];
-   // if ($method === 'OPTIONS') {
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method === 'OPTIONS') {
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        //header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    //}
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    }
+
 
     include_once '../../config/Database.php';
     include_once '../../models/Author.php';
@@ -49,30 +47,3 @@
         echo 'what the?';
         break;
     }
-
-function cors() {
-    
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-    
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    
-        exit(0);
-    }
-    
-    echo "You have CORS!";
-}
