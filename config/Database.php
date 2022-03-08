bbe83a28dc26c01748dd8e17504e346a274629e5
bryce hulett <bryce.hulett@hotmail.com>
2022-03-07 18:37:22 -0600

    debugging

--- config/Database.php
+++ config/Database.php
@@ -28,6 +28,7 @@
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully";
+        return $conn;
       }
       catch(PDOException $e)
       {
